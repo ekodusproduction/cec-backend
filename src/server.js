157c0902bead72923a4703cc5154f519da0e0294
jay-ekodus <jsardar@ekodusinc.com>
2023-08-07 10:48:06 +0530

    model changes-3

--- src/server.js
+++ src/server.js
@@ -5,6 +5,17 @@ import mongoose from "mongoose";
 import path from "path"
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
+import superAdminModel from "./Models/superAdminModel.js";
+import centerAdminModel from "./Models/centerAdminModel.js";
+import centerModel from "./Models/centerModel.js";
+import courseModel from "./Models/courseModel.js";
+import categoryModel from "./Models/courseCategoryModel.js";
+import qualificationModel from "./Models/qualificationModel.js";
+import studentModel from "./Models/studentModel.js";
+import orderModel from "./Models/orderModel.js";
+import paymentsModel from "./Models/paymentModel.js";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config({path:path.join(__dirname,"../.env") });
