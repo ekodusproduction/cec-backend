4888f2a240f38cc761c5dd699af6f0416d8002ff
jay-ekodus <jsardar@ekodusinc.com>
2023-08-18 17:49:16 +0530

    course fix-9

--- src/Auth/isSuperAdmin.js
+++ src/Auth/isSuperAdmin.js
@@ -4,7 +4,7 @@ export const isSuperAdmin = async (req, res, next) => {
   try {
     const superadmin = await superAdminModel.findById(req.id);
     if (!superadmin) {
-      return res.status(403).send({message:"Not authorised for this resource"}));
+      return res.status(403).send({message:"Not authorised for this resource"});
     }
     next();
   } catch (err) {
