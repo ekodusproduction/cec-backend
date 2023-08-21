17027c80ee466948657ea2dc1b57e3803f1b7ff5
jay-ekodus <jsardar@ekodusinc.com>
2023-08-21 12:16:25 +0530

    fix update admin-3

--- src/Routes/centerAdminRoutes.js
+++ src/Routes/centerAdminRoutes.js
@@ -4,24 +4,22 @@ import { verifyToken } from "../Auth/authorization.js";
 import {
   createcenterAdmin,
   getcenterAdmin,
-  updatecenterAdmin,
+  updateCenterAdmin,
   deletecenterAdmin,
   getAllcenterAdmin,
   getAllInactiveCenterAdmin,
   fileUpload,
 } from "../Controllers/centerAdminController.js";
-// import { loginCenteradmin } from "../Controllers/centerAdminController.js";
 import { isSuperAdmin } from "../Auth/isSuperAdmin.js";
 
 const router = Router();
-// router.route("/centeradmin/login").post(loginCenteradmin);
 router
   .route("/centeradmin")
   .post(verifyToken, createcenterAdmin)
   .delete(verifyToken, isSuperAdmin, deletecenterAdmin)
   .get(verifyToken, getcenterAdmin);
 
-router.route("/centeradmin/:centerAdminId").put(verifyToken, updatecenterAdmin);
+router.route("/centeradmin/:centerAdminId").put(verifyToken, updateCenterAdmin);
 router.route("/centeradmin/fileuploads").put(verifyToken, fileUpload);
 router.route("/centeradmin/all").get(verifyToken, getAllcenterAdmin);
 router
