4e609acbfa86583a09a19783c93466123b93c4af
jay-ekodus <jsardar@ekodusinc.com>
2023-08-22 10:20:02 +0530

    fix roll number-2

--- src/Routes/superAdminRoutes.js
+++ src/Routes/superAdminRoutes.js
@@ -6,7 +6,6 @@ import {
   loginSuperAdmin,
   updateSuperAdmin,
 } from "../Controllers/superAdminController.js";
-import { generateToken } from "../Auth/authentication.js";
 import { verifyToken } from "../Auth/authorization.js";
 import multer from "multer";
 import { isSuperAdmin } from "../Auth/isSuperAdmin.js";
