68ae3e500608f0ef1515d67be5d9afec4a49eb9f
jay-ekodus <jsardar@ekodusinc.com>
2023-08-24 10:44:51 +0530

    student updates change -4

--- src/Utils/errorHandler.js
+++ src/Utils/errorHandler.js
@@ -1,9 +1,12 @@
 export const handleErrors= async(err, req, res, next)=>{
-    if (err.code === 11000 && err.keyPattern.email === 1) {
-        return res.status(400).send({
-          message: `Email address '${err.keyValue.email}' is already in use. Please provide another value.`,
+    if (err.code === 11000 && err.keyPattern) {
+        const duplicateField = Object.keys(err.keyPattern)[0];
+        const duplicateValue = err.keyValue[duplicateField];
+    
+        return res.status(400).json({
+          message: `${duplicateField} '${duplicateValue}' is already in use. Please provide another value.`,
           status: 'fail',
         });
       }
-    
+      return res.status(500).send({message:err.message, status:500})
 }
